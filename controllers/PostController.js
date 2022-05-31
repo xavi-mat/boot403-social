22cc71a616d06957af8a471a5c6694d8367efe0d
Xavimat <xaviermatoses@gmail.com>
2022-05-31 22:07:05 +0200

    Pagination

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,4 +1,5 @@
 const { Post, User } = require("../models/");
+const PER_PAGE = 10;
 
 const PostController = {
     async create(req, res) {
@@ -40,11 +41,61 @@ const PostController = {
             }
             const title = new RegExp(req.params.title, 'i');
             const posts = await Post.find({ title })
-                .populate('author', {username:1, avatar:1});
+                .populate('author', { username: 1, avatar: 1 });
             return res.send(posts);
         } catch (error) {
             console.error(error);
-            return res.status(400).send({ msg: 'Error getting post' });
+            return res.status(400).send({ msg: 'Error getting posts' });
+        }
+    },
+    async getAll(req, res) {
+        try {
+            // Pagination
+            let page = req.params.page;
+            if (isNaN(page) || page < 1) {
+                return res.send({ msg: "Page must be a number greater than 0" });
+            }
+            const total = await Post.count();
+            const maxPages = Math.ceil(total / PER_PAGE);
+            page = Math.min(page, maxPages);
+            const posts = await Post.find()
+                .limit(PER_PAGE)
+                .skip(PER_PAGE * (page - 1))
+                .populate('author', { username: 1, avatar: 1 })
+                .populate('comments');
+            res.send({ msg: "All posts", total, page, maxPages, posts });
+        } catch (error) {
+            console.error(error);
+            return res.status(400).send({ msg: 'Error getting posts' });
+        }
+    },
+    async update(req, res) {
+        try {
+            const updatedPost = {
+                title: req.body.title,
+                body: req.body.body,
+                image: req.body.image
+            };
+            const post = await Post.findOneAndUpdate(
+                { _id: req.params._id, author: req.user._id },
+                updatedPost,
+                { new: true }
+            );
+            return res.send({ msg: "Post updated", post });
+        } catch (error) {
+            console.error(error);
+            return res.status(400).send({ msg: 'Error updating post' });
+        }
+    },
+    async delete(req, res) {
+        try {
+            const post = await Post.findOneAndDelete(
+                { _id: req.params._id, author: req.user._id }
+            );
+            res.send({ msg: "Post deleted", post });
+        } catch (error) {
+            console.error(error);
+            return res.status(400).send({ msg: 'Error deleting post' });
         }
     }
 
--- routes/posts.js
+++ routes/posts.js
@@ -7,5 +7,8 @@ const router = express.Router();
 router.post('/', authentication, PostController.create);
 router.get('/id/:_id', PostController.getById);
 router.get('/title/:title', PostController.getByTitle);
+router.get('/page/:page', PostController.getAll);
+router.put('/id/:_id', authentication, PostController.update);
+router.delete('/id/:_id', authentication, PostController.delete);
 
 module.exports = router;
