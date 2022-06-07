0d0a3f6628ac4dabcb517f2b38ee10376b5487cb
Xavimat <xaviermatoses@gmail.com>
2022-06-07 11:34:50 +0200

    Last commits in mini-frontend

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 const { dbConnection } = require("./config/config");
 const { typeError } = require("./middleware/errors");
+const swaggerUI = require("swagger-ui-express");
+const docs = require("./docs/index");
 
 app.use(express.json());
 app.use(express.static('public'));
@@ -14,6 +16,8 @@ dbConnection();
 app.use('/users', require('./routes/users'));
 app.use('/posts', require('./routes/posts'));
 app.use('/comments', require('./routes/comments'));
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(docs));
 
 app.use(typeError);
 
